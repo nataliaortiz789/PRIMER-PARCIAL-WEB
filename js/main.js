5552fc7d7ac6a4670bdead7d35c729b666456079
Natalia Ortiz <nataliaoa@ufps.edu.co>
2021-04-12 10:33:14 -0500

    tercer commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 $("#selh1").click(function (event) {
-  $("#contenedor").load("selecciones/partido.html");
+    $("#contenedor").load("selecciones/partido.html");
 });
 
 $("#selh2").click(function (event) {
-  $("#contenedor").load("selecciones/contacto.html");
+    $("#contenedor").load("selecciones/contacto.html");
 });
 
 $("#selh3").click(function (event) {
-  $("#contenedor").load("selecciones/colombia.html");
+    $("#contenedor").load("selecciones/colombia.html");
 });
 
-const tabla = document.querySelector('#lista-usuarios tbody');
+const tabla = document.querySelector('#lista-usuarios');
+
 function cargarUsuarios() {
-  fetch("usuarios.json")
-    .then((respuesta) => respuesta.json()) 
-    .then((usuarios) => {
-      usuarios.forEach((usuario) => {
-        const row = document.createElement("tr");
-        row.innerHTML += `
-                    <td>${usuario.id}</td>
-                    <td>${usuario.name}</td>
-                    <td>${usuario.email}</td>
-                    <td>${usuario.company.name}</td>
-                `;
-        tabla.appendChild(row);
-      });
-    }) // Aquí mostramos dicha información
-    .catch((error) => console.log("Hubo un error : " + error.message));
+    fetch("http://demo6497253.mockable.io/noticias")
+        .then((respuesta) => respuesta.json())
+        .then((usuarios) => {
+            usuarios.forEach((usuario) => {
+                const row = document.createElement("tr");
+                row.innerHTML += `
+                <h5>${json[i].descripcion}</h5>
+                <p>${json[i].detalle}</p>
+                <img src='${json[i].img}'></img></div><hr>`;
+                tabla.appendChild(row);
+            });
+        }) // Aquí mostramos dicha información
+        .catch((error) => console.log("Hubo un error : " + error.message));
 }
 
 cargarUsuarios();
